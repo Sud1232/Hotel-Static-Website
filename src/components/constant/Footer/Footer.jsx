a3a83f46309872f8bc62c0277cb4f647a0d67adc
Sud1232 <sudarshanpatekar27@gmail.com>
2024-08-05 18:13:30 +0530

    footer arrow positioned properly

--- src/components/constant/Footer/Footer.jsx
+++ src/components/constant/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import "../../../../src/App.css"
 function Footer() {
   return (
     <>
-      <div className="bg-[#14274A] xl:w-16 xl:h-12 xl:flex xl:rotate-45 xl:m-auto xl:translate-y-8 xl:z-0 xl:relative xsm:relative xsm:w-20 xsm:h-20 xsm:rotate-45 xsm:ml-[580px] xsm:translate-y-10"></div>
+      <div className="bg-[#14274A] xl:w-16 xl:h-12 xl:flex xl:rotate-45 xl:m-auto xl:ml-[46.5rem] xl:translate-y-8 xl:z-0 xl:relative xsm:relative xsm:w-20 xsm:h-20 xsm:rotate-45 xsm:ml-[600px] xsm:translate-y-10"></div>
       <div className="bg-[#14274A] xl:z-1 xl:w-full xl:absolute xsm:absolute text-white xl:flex xl:justify-around  xl:p-10 xsm:w-[1270px]">
         <div className=" xsm:w-[1271px] xsm:flex xsm:flex-col xsm:text-center xl:flex xl:flex-col font-[adobe-garamond-pro] xsm:text-5xl xsm:p-12 xl:w-auto xl:text-base xl:text-left">
           <p className="xl:text-2xl font-bold ">LUXURY</p>
